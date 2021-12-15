dc3cc14efe5c677e1db025a0ed412c2179e7233d
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-16 00:26:32 +0100

    created a function that shows the Todos in the browser

--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,15 @@
-<?php ?>
+<?php
+$db = new PDO('mysql:host=mysql;dbname=onurdb', 'onur', 'secret');
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+function showTodo() {
+    global $db;
+    $rows = $db->query('SELECT * FROM todoList ORDER BY todo');
+
+    foreach ($rows as $row) {
+        var_dump($row);
+    }
+}
+
+?>
