c89c8866c091db872fed19fd25c31c6274b3e847
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-16 17:56:18 +0100

    deleted my html and create.php

--- app/public/index.php
+++ app/public/index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="create.php" method="POST">
-        <input type="text" placeholder="todo" name="todo">
-        <input type="text" placeholder="id" name="id">
-        <input type="submit" value="Create" name="changeTodo">
-    </form>
-    
-</body>
-</html>
+
 
 <?php
 $db = new PDO('mysql:host=mysql;dbname=onurdb', 'onur', 'secret');
