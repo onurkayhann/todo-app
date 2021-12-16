d51c38b8cd433c2866c9006d1c09fee90e4c4af0
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-16 19:46:36 +0100

    renamed create to send.php

--- app/public/send.php
+++ app/public/send.php
@@ -11,9 +11,11 @@ $db = new DB();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Todo</title>
 </head>
 <body>
+
+<form action=""></form>
     
 </body>
 </html>
