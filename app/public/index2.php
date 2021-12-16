8c5233e4e5623fa1235bf40a52bc8c0ada12c6e6
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-17 00:17:59 +0100

    created a form and a body tag inside my index2.php file

--- app/public/index2.php
+++ app/public/index2.php
@@ -5,3 +5,22 @@ $db = new DB();
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+</head>
+<body>
+
+<form action="create.php" method="POST">
+    <input type="text" placeholder="Name" name="name">
+    <input type="text" placeholder="Todo" name="todo">
+    <input type="submit" value="Create" name="createTodo">
+</form>
+    
+</body>
+</html>
+
