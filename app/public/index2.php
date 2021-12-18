86e11a216ce7b51c6e39f18e7d57dcb67922b030
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-18 18:16:52 +0100

    created a style.css file and connected it to my working files

--- app/public/index2.php
+++ app/public/index2.php
@@ -12,6 +12,7 @@ $db = new DB();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
