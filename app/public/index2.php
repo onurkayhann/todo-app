ad24948371fe4f64c417b1c78ac5545fd850c172
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-20 23:55:14 +0100

    added some colors and styles to my Todo

--- app/public/index2.php
+++ app/public/index2.php
@@ -24,7 +24,7 @@ if(isset($_GET['delete'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>u04-TODO-APP</title>
-    <link rel="stylesheet" href="css/mains.css">
+    <link rel="stylesheet" href="css/main.css">
 
 </head>
 <body>
