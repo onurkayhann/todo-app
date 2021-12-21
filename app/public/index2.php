80080bcaf7a484973a2db5bc47d94579703a8f74
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-12-21 14:59:32 +0100

    deleted some unnecessary codes and comments

--- app/public/index2.php
+++ app/public/index2.php
@@ -18,8 +18,6 @@ if(isset($_GET['delete'])) {
 // Mark as done
 if(isset($_GET['done'])) {
   $id = $_GET['done'];
-
-  // $db = new DB();
   $db->markTodo($id);
 }
 
@@ -45,11 +43,6 @@ if(isset($_GET['done'])) {
     <input type="submit" value="Create" name="createTodo">
   </form>
 
-  <!-- <h1>Delete</h1>
-  <form method="POST">
-    <input type="text" placeholder="id" name="id">
-    <input type="submit" value="Delete" name="deleteTodo">
-  </form> -->
 
   <h1>Update</h1>
   <form action="update.php" method="POST">
@@ -68,16 +61,7 @@ if(isset($_GET['done'])) {
   }
   ?>
 
-  <!-- <a class='done-btn done' href='done.php?as=done&id={$i['id']}'><ion-icon name='checkbox-outline'></ion-icon></a> -->
-  <!-- " " . $i['id'] . ". " . $i['todo'] . " - " . $i['user'] -->
   
-  <!-- <?php foreach($todoList as $i): ?>
-    <div class="todo-container">
-    <a class='done-btn' href='done.php?as=done&id=<?php echo $i['id']?>'><ion-icon name='checkbox-outline'></ion-icon></a>
-    <p class="todo <?php echo $i['done'] ? ' done' : '';?>"><?php echo $i['id'] . ". " . " " . $i['todo'] . " - " . $i['user'] . " " ?></p>
-    <a class='delete-btn' href='index2.php?delete=<?php echo $i['id'] ?>'>Delete<ion-icon name='close'></ion-icon></a>
-    </div>
-  <?php endforeach; ?> -->
 
 
 
